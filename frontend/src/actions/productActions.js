34f62d0bbf844a5c92d95a819113fa431c6dfa9a
abhinav0031 <abhinav16100@gmail.com>
2021-03-06 23:36:18 +0530

    testing my patience

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,7 +1,10 @@
 import {
     PROUDUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PROUDUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstant'
 import axios from 'axios'
 export const listProducts=()=>async (dispatch)=>{
@@ -20,4 +23,22 @@ export const listProducts=()=>async (dispatch)=>{
             :error.message
         })
      }
+}
+export const listProductDetails=(id)=>async (dispatch)=>{
+    try{
+        dispatch({type:PROUDUCT_DETAILS_REQUEST})
+        const {data}=await axios.get(`/api/products/${id}`)
+      
+        dispatch({
+            type:PRODUCT_DETAILS_SUCCESS,
+            payload:data
+        })
+    }  catch(error){   
+       dispatch({
+           type:PRODUCT_DETAILS_FAIL,
+           payload:error.response && error.response.data.message
+           ? error.response.data.message
+           :error.message
+       })
+    }
 }
