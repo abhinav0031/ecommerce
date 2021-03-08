e9b0fd593846df7c35894d5d2eeadbb61550aeeb
abhinav0031 <abhinav16100@gmail.com>
2021-03-08 14:05:42 +0530

    update profile endpoint added

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import UserSerializer, UserSerializerWithToken
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
@@ -44,6 +44,21 @@ def registerUser(request):
             'detail': 'User with this email already exist'
         }
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many=False)
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+    user.save()
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
