8a1620c930c40e1a80ff58bff14b94fbfc9e8b4c
abhinav0031 <abhinav16100@gmail.com>
2021-03-09 23:49:00 +0530

    added order details on user profile screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -15,7 +15,7 @@ import
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_RESET
 } from '../constants/userConstant'
-
+import {ORDER_LIST_MY_RESET} from '../constants/orderConstants'
 import axios from 'axios'
 export const login=(email,password)=>async(dispatch)=>{
     try{
@@ -57,6 +57,9 @@ export const logout=()=>(dispatch)=>{
     })
     dispatch({
         type:USER_DETAILS_RESET
+    })
+    dispatch({
+        type:ORDER_LIST_MY_RESET
     })
 }
 
