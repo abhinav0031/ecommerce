f941bf67078f98a5d70065e4a9ee9075819de6cd
abhinav0031 <abhinav16100@gmail.com>
2021-03-10 10:38:02 +0530

    userListScreen made

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {productListReducer,productDetailsReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
-import {userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer} from './reducers/userReducers'
+import {userListReducer,userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer} from './reducers/userReducers'
 import {orderCreateReducer,orderDetailsReducer,orderPayReducer,orderListMyReducer} from './reducers/orderReducers'
 
 const reducer=combineReducers({
@@ -11,6 +11,7 @@ const reducer=combineReducers({
     userRegister:userRegisterReducer,
     userDetails:userDetailsReducer,
     userUpdateProfile:userUpdateProfileReducer,
+    userList:userListReducer,
     productList:productListReducer,
     productDetails:productDetailsReducer,
     cart:cartReducer,
