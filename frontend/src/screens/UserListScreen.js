5130385ca46e08a96ba53166e6c91aaf58e28ab9
abhinav0031 <abhinav16100@gmail.com>
2021-03-11 13:00:13 +0530

    added product list component adminuser can also delete product now

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -24,7 +24,7 @@ function UserListScreen({history}) {
             history.push('/login')
         }
         
-    },[dispatch,history,successDelete])
+    },[dispatch,history,successDelete,userInfo])
 
 
     const deleteHandler=(id)=>{
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import {productListReducer,productDetailsReducer} from './reducers/productReducers'
+import {productListReducer,productDetailsReducer,productDeleteReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
 import {userListReducer,userDeleteReducer,userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer,userUpdateReducer} from './reducers/userReducers'
 import {orderCreateReducer,orderDetailsReducer,orderPayReducer,orderListMyReducer} from './reducers/orderReducers'
@@ -17,7 +17,7 @@ const reducer=combineReducers({
 
     productList:productListReducer,
     productDetails:productDetailsReducer,
-    
+    productDelete:productDeleteReducer,
     cart:cartReducer,
     
     orderCreate:orderCreateReducer,
